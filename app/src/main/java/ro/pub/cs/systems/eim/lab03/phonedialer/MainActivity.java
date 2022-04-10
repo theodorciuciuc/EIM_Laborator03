dafc3633c30849721b903c0c046187334ddc3b8a
theodorciuciuc <ciuciuc.teodor@gmail.com>
2022-04-10 18:36:35 +0300

    changes for lab4

--- app/src/main/java/ro/pub/cs/systems/eim/lab03/phonedialer/MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/lab03/phonedialer/MainActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             R.id.star,
             R.id.pound
     };
+    private Contacts contacts = new Contacts();
     private ButtonTapListener buttonTapListener = new ButtonTapListener();
     private DeleteChar deleteChar = new DeleteChar();
     private Call call = new Call();
@@ -48,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.backspace).setOnClickListener(deleteChar);
         findViewById(R.id.call).setOnClickListener(call);
         findViewById(R.id.hangup).setOnClickListener(hangup);
+        findViewById(R.id.contacts).setOnClickListener(contacts);
         for (int i = 0; i < 12; i++) {
             Button button = findViewById(ids[i]);
             button.setOnClickListener(buttonTapListener);
@@ -92,6 +95,29 @@ public class MainActivity extends AppCompatActivity {
                 intent.setData(Uri.parse("tel:" + phoneNumber.getText().toString()));
                 startActivity(intent);
             }
+        }
+    }
+
+    private class Contacts implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Log.d("ceva", "altceva");
+            String phone = phoneNumber.getText().toString();
+            if (phone.length() > 0) {
+                Intent intent = new Intent("ro.pub.cs.systems.eim.lab04.contactsmanager.intent.action.MainActivity");
+                intent.putExtra("ro.pub.cs.systems.eim.lab04.contactsmanager.PHONE_NUMBER_KEY", phone);
+                startActivityForResult(intent, 2022);
+            } else {
+                Toast.makeText(getApplication(), getResources().getString(R.string.phone_error), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 2022) {
+            Toast.makeText(this, "Activity returned with result " + resultCode, Toast.LENGTH_LONG).show();
         }
     }
 }
